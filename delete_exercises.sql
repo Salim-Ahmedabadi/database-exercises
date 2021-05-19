f1eb872d8e9bac86dec1193bd1ecc5edf391d92b
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-19 17:31:59 -0500

    feat: Finished Basic Statements Exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,2 +1,11 @@
 USE codeup_test_db;
-DELETE FROM albums WHERE id = 4;
+SELECT 'Albums released after 1991!' AS 'Info';
+DELETE FROM albums WHERE release_date > 1991;
+
+
+SELECT 'Albums with the genre ''disco''!' AS 'Info';
+DELETE FROM albums WHERE genre = 'disco';
+
+
+SELECT 'Albums with the artist ''The Beatles''!' AS 'Info';
+DELETE FROM albums WHERE artist = 'The Beatles';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,4 +1,27 @@
 USE codeup_test_db;
+
+SELECT 'All albums in your table.!' AS 'Info';
+SELECT * FROM albums;
 UPDATE albums
-SET genre  = 'Rock', genre = 'Pop'
-WHERE id = 4;
+SET sales = sales * 10;
+SELECT 'New Albums 10 times more popular!' AS 'Info';
+SELECT * FROM albums;
+
+
+
+SELECT 'All albums released before 1980!' AS 'Info';
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = release_date - 100 WHERE release_date < 1980;
+SELECT 'Released albums in the 1800s!' AS 'Info';
+SELECT * FROM albums WHERE release_date BETWEEN 1800 AND 1899;
+
+
+SELECT 'All albums by Michael Jackson!' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT 'All albums by Michael Jackson changed to Peter Jackson!' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
+
+
