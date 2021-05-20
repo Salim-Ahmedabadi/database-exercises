f9b392796625e0bd76eda101706619b80dcf5eb8
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-20 13:59:46 -0500

    feat: Finished Where Exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,6 @@
+USE employees;
+SELECT * FROM employees WHERE first_name LIKE 'Vidya,Irene' ORDER BY first_name ASC;
+
+
+
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -7,3 +7,46 @@ SELECT * FROM employees WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
+-- PART 2
+SELECT first_name, last_name
+FROM employees
+Where first_name
+   OR ('Irena', 'Vidya', 'Maya');
+
+SELECT first_name, last_name
+FROM employees
+Where first_name
+   OR ('Irena', 'Vidya', 'Maya')
+    AND gender = 'M';
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+    like 'e%'
+   OR '%e';
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+    like 'e%'
+  AND '%e';
+
+SELECT first_name, last_name
+FROM employees
+Where last_name
+    like '%q%'
+  AND NOT '%qu%';
+
+
+
+
+
+
+
+
+
+
+
+
+-- 5.
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
