a7a81ea66dff293994d4f36799090f9c11a0d032
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-20 14:46:56 -0500

    feat: Worked on Limit Exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,16 +3,16 @@ USE employees;
 
 SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no DESC;
 
-SELECT * FROM employees WHERE last_name LIKE '%q%';
-
-SELECT * FROM employees WHERE frst_name = 'Irene' OR first_name = 'Maya' OR first_name = 'Vidya'
-AND gender LIKE 'M';
-
-SELECT * FROM employees WHERE first_name LIKE 'e%' OR first_name LIKE 'e%';
-
-SELECT * FROM employees WHERE first_name LIKE 'e%' AND first_name LIKE 'e%';
-
-SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%'
+-- SELECT * FROM employees WHERE last_name LIKE '%q%';
+--
+-- SELECT * FROM employees WHERE frst_name = 'Irene' OR first_name = 'Maya' OR first_name = 'Vidya'
+-- AND gender LIKE 'M';
+--
+-- SELECT * FROM employees WHERE first_name LIKE 'e%' OR first_name LIKE 'e%';
+--
+-- SELECT * FROM employees WHERE first_name LIKE 'e%' AND first_name LIKE 'e%';
+--
+-- SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%'
 
 
 
