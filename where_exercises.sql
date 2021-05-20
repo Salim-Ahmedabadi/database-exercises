e8426b71fe1d93660c8a24410358b7cec85c9eaf
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-05-20 11:15:30 -0500

    feat: Finished Part One of Clauses Sql Exercis

---
+++ where_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
